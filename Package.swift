4309bf3cbfa7334fc41960c1504201fc84c66e8e
Juan Alvarez <jlalvarez18@gmail.com>
2017-11-03 12:55:16 -0500

    updated project to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "HelloWorld",
+    products: [
+        .executable(name: "App", targets: ["App"]),
+        ],
     dependencies: [
-        .Package(url: "https://github.com/swiftlyio/swiftly.git", majorVersion: 1)
-    ]
+        .package(url: "https://github.com/swiftlyio/swiftly.git", from: "1.0.0")
+        ],
+    targets: [
+        .target(name: "App", dependencies: ["Swiftly"]),
+        ]
 )
